5042c56b2d7dc1cf20131e460108e932c5e0f925
Israel Gonzalez <igonzalezsa@aeromexico.com>
2021-08-25 23:06:13 -0500

    Create table on mydb schema

--- update.sql
+++ update.sql
@@ -7,55 +7,20 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Table `report_palace`.`status_report`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `report_palace`.`status_report` (
-  `state_report_id` INT NOT NULL AUTO_INCREMENT,
-  `decription` VARCHAR(1024) NULL DEFAULT NULL,
-  `status` VARCHAR(512) NOT NULL,
-  `is_active` TINYINT(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`state_report_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
--- Table `report_palace`.`reports_created`
+-- Table `mydb`.`subreports`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `report_palace`.`reports_created` (
-  `report_created_id` NVARCHAR(36) NOT NULL,
-  `creation_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `is_active` TINYINT(1) NOT NULL DEFAULT '1',
-  `state_report_id` INT NOT NULL,
-  PRIMARY KEY (`report_created_id`, `state_report_id`),
-  UNIQUE INDEX `report_created_id_UNIQUE` (`report_created_id` ASC) ,
-  INDEX `fk_reports_created_status_report_idx` (`state_report_id` ASC) ,
-  CONSTRAINT `fk_reports_created_status_report`
-    FOREIGN KEY (`state_report_id`)
-    REFERENCES `report_palace`.`status_report` (`state_report_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `report_palace`.`report_bodies`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `report_palace`.`report_bodies` (
+CREATE TABLE IF NOT EXISTS `mydb`.`subreports` (
+  `idsubreports` BIGINT NOT NULL AUTO_INCREMENT,
+  `scheme_identifier` NVARCHAR(36) NOT NULL,
+  `params` JSON NOT NULL,
   `report_body_id` NVARCHAR(36) NOT NULL,
-  `report_created_id` NVARCHAR(36) NOT NULL,
-  `subreport_identifier` NVARCHAR(36) NULL,
-  PRIMARY KEY (`report_body_id`, `report_created_id`),
-  UNIQUE INDEX `report_body_id_UNIQUE` (`report_body_id` ASC) ,
-  INDEX `fk_report_bodies_reports_created1_idx` (`report_created_id` ASC) ,
-  CONSTRAINT `fk_report_bodies_reports_created1`
-    FOREIGN KEY (`report_created_id`)
-    REFERENCES `report_palace`.`reports_created` (`report_created_id`)
+  PRIMARY KEY (`idsubreports`, `report_body_id`),
+  INDEX `fk_subreports_report_bodies_idx` (`report_body_id` ASC) ,
+  CONSTRAINT `fk_subreports_report_bodies`
+    FOREIGN KEY (`report_body_id`)
+    REFERENCES `report_palace`.`report_bodies` (`report_body_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+ENGINE = InnoDB;
