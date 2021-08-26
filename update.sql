176618c32a9634e5a195f7fee1deb174180fcb8d
Israel Gonzalez <igonzalezsa@aeromexico.com>
2021-08-25 23:01:42 -0500

    Update cause VISIBLE clause

--- update.sql
+++ update.sql
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS `report_palace`.`reports_created` (
   `is_active` TINYINT(1) NOT NULL DEFAULT '1',
   `state_report_id` INT NOT NULL,
   PRIMARY KEY (`report_created_id`, `state_report_id`),
-  UNIQUE INDEX `report_created_id_UNIQUE` (`report_created_id` ASC) VISIBLE,
-  INDEX `fk_reports_created_status_report_idx` (`state_report_id` ASC) VISIBLE,
+  UNIQUE INDEX `report_created_id_UNIQUE` (`report_created_id` ASC) ,
+  INDEX `fk_reports_created_status_report_idx` (`state_report_id` ASC) ,
   CONSTRAINT `fk_reports_created_status_report`
     FOREIGN KEY (`state_report_id`)
     REFERENCES `report_palace`.`status_report` (`state_report_id`)
@@ -49,8 +49,8 @@ CREATE TABLE IF NOT EXISTS `report_palace`.`report_bodies` (
   `report_created_id` NVARCHAR(36) NOT NULL,
   `subreport_identifier` NVARCHAR(36) NULL,
   PRIMARY KEY (`report_body_id`, `report_created_id`),
-  UNIQUE INDEX `report_body_id_UNIQUE` (`report_body_id` ASC) VISIBLE,
-  INDEX `fk_report_bodies_reports_created1_idx` (`report_created_id` ASC) VISIBLE,
+  UNIQUE INDEX `report_body_id_UNIQUE` (`report_body_id` ASC) ,
+  INDEX `fk_report_bodies_reports_created1_idx` (`report_created_id` ASC) ,
   CONSTRAINT `fk_report_bodies_reports_created1`
     FOREIGN KEY (`report_created_id`)
     REFERENCES `report_palace`.`reports_created` (`report_created_id`)
